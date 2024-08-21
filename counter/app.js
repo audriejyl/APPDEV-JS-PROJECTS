6be6635ab2c0f26a70df4c53fb5f9cef682d33d3
audriejyl <20193616@s.ubaguio.edu>
2024-08-21 16:33:06 +0800

    Add files via upload

---
+++ counter/app.js
@@ -0,0 +1,37 @@
+let count = 0;
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList
+        if (styles.contains('decrease')){
+            count--
+        }
+        else if(styles.contains('increase')){
+            count++
+        }
+        else if(styles.contains('decreaseFive')){
+            count -= 5
+        }
+        else if(styles.contains('increaseFive')){
+            count += 5
+        }
+        else if(styles.contains('random')){
+            count = Math.floor(Math.random() * 200) - 100;
+        }
+        else{
+            count = 0
+        }
+        if(count > 0){
+            value.style.color = "green"
+        }
+        if(count < 0){
+            value.style.color = "red"
+        }
+        if(count==0){
+            value.style.color = "black"
+        }
+        value.textContent = count
+    })
+   
+});
