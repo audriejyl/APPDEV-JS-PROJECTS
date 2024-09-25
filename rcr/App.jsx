fa97ec85b426ab99bb4badb953350d8563eae2be
audriejyl <20193616@s.ubaguio.edu>
2024-09-25 15:51:00 +0800

    Add files via upload

---
+++ rcr/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './Header'
+import Footer from './Footer'
+import Counter from './Counter'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Counter />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ rcr/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <h1><center>My Counter Project</center></h1>
+            <hr/>
+        </header>
+    )
+}
