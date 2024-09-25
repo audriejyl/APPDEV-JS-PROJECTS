f133cdee2ac74907127296538cf93e964c2f08e5
audriejyl <20193616@s.ubaguio.edu>
2024-09-25 15:45:51 +0800

    Delete src/Header.jsx

--- src/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return(
-        <header>
-            <h1><center>My Color Picker: A Web Application</center></h1>
-            <hr/>
-        </header>
-    )
-}
